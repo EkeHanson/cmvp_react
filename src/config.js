f6b688ba20be96780aff58d6ca2c5e3f528417c7
Abraham Ekene-onwon Hanson <ekenehanson@gmail.com>
2024-07-17 09:59:53 +0100

    Done with the company profile page

--- src/config.js
+++ src/config.js
@@ -1,5 +1,6 @@
 // src/config.js
 
-const SERVER_HOSTNAME = 'http://127.0.0.1:9090'; // Replace with your server hostname
+//const SERVER_HOSTNAME = 'http://127.0.0.1:9090'; // Replace with your server hostname
+const SERVER_HOSTNAME = 'https://cmvp-api.onrender.com'; // Replace with your server hostname
 
 export default SERVER_HOSTNAME;
